aeb9ed1a3bf9fcebaf202a88a91db28da360401b
Samusaa21 <78561008+Samusaa21@users.noreply.github.com>
2021-05-02 19:42:41 -0500

    Update capturandorostros.py

--- capturandorostros.py
+++ capturandorostros.py
@@ -10,8 +10,8 @@ if not os.path.exists(personPath):
     print('Carpeta creada: ',personPath)
     os.makedirs(personPath)
 
-#cap = cv2.VideoCapture(0)
-cap = cv2.VideoCapture('C:/Users/jpaon/OneDrive/Escritorio/Reconocimiento Facial/Jeimmy prueba.mp4')
+cap = cv2.VideoCapture(0)
+#cap = cv2.VideoCapture('C:/Users/jpaon/OneDrive/Escritorio/Reconocimiento Facial/Jeimmy prueba.mp4')
 
 faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
 count = 0
